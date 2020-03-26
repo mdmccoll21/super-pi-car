2a4a9b84d28b0f64076c40a82d3551d16ab84885
mdmccoll21 <62043243+mdmccoll21@users.noreply.github.com>
2020-03-25 21:59:32 -0400

    Create control.php

---
+++ control.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<?php include 'includes/head.php'; ?>
+</head>
+<body>
+
+<?php include 'includes/navbar.php'; ?>
+
+	<div class="row mt-5">
+		<div class="col-12 text-center mt-5">
+			<h3>Control It</h3>
+	</div>
+</div>
+
+<!--- Two Column Section -->
+<div class="container-fluid padding">
+<div class="row padding">
+	<div class="col-md-12 col-lg-6">
+		<h2>Key takeaways from this project...</h2>
+		<p>Learning the ways of project management for the real world.</p>
+		<p>Sharpening time management skills.</p>
+		<p>Being equipped with learned objectives from the Capstone course.</p>
+		<p>Acquired hands-on experience with coding and programming.</p>
+		<br>
+	</div>
+	<div class="col-lg-6">
+		<img src="img/grad.jpg" class="img-fluid">
+	</div>
+</div>
+</div>
+
+<?php include 'includes/footer.php'; ?>
+
+<?php include 'includes/scripts.php'; ?>
+
+</body>
+</html>
+
